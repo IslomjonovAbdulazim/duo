6bc315fbfefcb05172fd1bec0c78e07c576ea219
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-11-06 14:42:51 +0500

    impp

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/')({
   beforeLoad: () => {
-    throw redirect({ to: '/learning-centers' })
+    throw redirect({ to: '/zehnly-duo' })
   },
 })
