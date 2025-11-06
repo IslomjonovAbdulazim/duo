e6e1866f36d4f5be1d36bb0423c4647f02420ded
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-11-06 18:49:16 +0500

    aaaaaa

---
+++ dist/assets/route-B5CXa4-K.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-BUetp_wP.js";function o({children:n}){return t.jsx("div",{className:"min-h-screen w-full",children:n??t.jsx(e,{})})}const i=o;export{i as component};
